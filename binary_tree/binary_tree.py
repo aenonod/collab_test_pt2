0258bc32e8ed288f871bd62c048e55aa686e594a
Abegail E. Nonod <abegail.nonod13@gmail.com>
2025-12-23 20:17:48 +0800

    feat(binary_tree.py): gui bt label and frame (#13)
    * feat(array.py): added new file
    
    * docs(array.py): add pseudocode
    
    * feat(array.py): add pseudocode
    
    * feat(array.py): add print statement
    
    * chore(binary_tree_folder): add new folder and empty file
    
    * feat(binary_tree.py): add window background
    
    * feat(binary_tree.py): gui bt label and frame

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,12 +1,34 @@
+import tkinter as tk
 from tkinter import *
 
-window = Tk()
-window.geometry("1920x1080")
-window.title("Structura")
+class TreeGUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1920x1080")
+        self.title("Structura")
 
-bg_image = PhotoImage(file="binary_tree/background.png")
+        self.setup_background()
+        self.setup_label()
 
-bg_label = Label(window, image=bg_image)
-bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+    def setup_background(self):
+        self.bg_image = PhotoImage(file="binary_tree/background.png")
+        bg_label = Label(self, image=self.bg_image)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-window.mainloop()
+    def setup_label(self):
+        self.label_frame = Frame(self,
+                                 bg = "#6e7bb2",
+                                 width=850, height=92,
+                                 bd = 8, relief = "solid")
+        self.label_frame.place(relx=0.99, rely=0.02, anchor="ne")
+        self.label_text = Label(self.label_frame,
+                                text="Binary Tree",
+                                font=("Press Start 2P", 20, "bold"),
+                                fg = "white",
+                                bg="#6e7bb2")
+        self.label_text.pack(padx=30, pady=15)
+
+
+if __name__ == "__main__":
+    app = TreeGUI()
+    app.mainloop()
